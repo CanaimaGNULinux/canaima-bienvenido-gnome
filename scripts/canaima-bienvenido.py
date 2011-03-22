d387b4140c65a947d3dead9f1cff5504365ba38c
Carlos Guerrero <guerrerocarlos@gmail.com>
2011-03-22 12:07:25 -0430

    Corrigiendo errores reportados en el funcionamiento

--- scripts/canaima-bienvenido.py
+++ scripts/canaima-bienvenido.py
@@ -17,125 +17,99 @@ try:
         import gtk.glade
 except:
         sys.exit(1)
-        
-def read_output(view, buffer, command):
-        stdin, stdouterr = os.popen4(command)
-        while 1:
-                line = stdouterr.readline()
-                if not line:
-                        break
-                gtk.gdk.threads_enter()
-                iter = buffer.get_end_iter()
-                buffer.place_cursor(iter)
-                buffer.insert(iter, str(line))
-                view.scroll_to_mark(buffer.get_insert(), 0.1)
-                gtk.gdk.threads_leave() 
-               
-def abrir_paralelo(view, buffer, comando):
-                os.system(comando)
 
-class HellowWorldGTK:
-        gobject.threads_init()
-        gtk.gdk.threads_init()
+class canaimabienvenido:
+    encoding = locale.getpreferredencoding()
+    utf8conv = lambda x : unicode(x, encoding).encode('utf8')
 
-        encoding = locale.getpreferredencoding()
-        utf8conv = lambda x : unicode(x, encoding).encode('utf8')
+    fuente = ""
+    destino = ""
+  
+    def __init__(self):
+            #Set the Glade file
+            self.gladefile = "/usr/share/canaima-bienvenido/interfaz.glade"
 
-        fuente = ""
-        destino = ""
-      
-        def __init__(self):
-                #Set the Glade file
-                self.gladefile = "/usr/share/canaima-bienvenido/interfaz.glade"
+            self.wTree = gtk.glade.XML(self.gladefile) 
 
-                self.wTree = gtk.glade.XML(self.gladefile) 
+            #Create our dictionay and connect it
+            dic = { "on_cerrar_clicked" : self.cerrar,
+                    "on_b1_clicked" : self.b1_clicked,
+                    "on_b2_clicked" : self.b2_clicked,
+                    "on_b3_clicked" : self.b3_clicked,
+                    "on_b4_clicked" : self.b4_clicked,
+                    "on_b5_clicked" : self.b5_clicked,
+                    "on_b6_clicked" : self.b6_clicked,
+                    "on_b7_clicked" : self.b7_clicked,
+                    "on_b8_clicked" : self.b8_clicked,
+                    "on_mostrar_toggled" : self.checkmostrar,
+                    "destroy" : self.cerrar,
+                    "on_MainWindow_destroy" : self.cerrar }
+            
+            self.wTree.get_widget("window1").show(); 
+            self.wTree.get_widget("window1").connect("delete-event",self.cerrar); 
+            
+            self.wTree.signal_autoconnect(dic)
+            gtk.main()
 
-                #Create our dictionay and connect it
-                dic = { "on_cerrar_clicked" : self.btnHelloWorld_clicked,
-                        "on_b1_clicked" : self.b1_clicked,
-                        "on_b2_clicked" : self.b2_clicked,
-                        "on_b3_clicked" : self.b3_clicked,
-                        "on_b4_clicked" : self.b4_clicked,
-                        "on_b5_clicked" : self.b5_clicked,
-                        "on_b6_clicked" : self.b6_clicked,
-                        "on_b7_clicked" : self.b7_clicked,
-                        "on_b8_clicked" : self.b8_clicked,
-                        "on_mostrar_toggled" : self.checkmostrar,
-                        "on_MainWindow_destroy" : gtk.main_quit }
-                
-                self.wTree.get_widget("window1").show(); 
-                
-                self.wTree.signal_autoconnect(dic)
-                gtk.main()
-                
-	def btnHelloWorld_clicked(self, widget):
-                print "Hasta la proxima!"
-                self.wTree.get_widget("window1").hide();
-		exit(0);
+    #Get the Main Window, and connect the "destroy" event
+ 
+    def checkmostrar(self, widget):
+        print self.wTree.get_widget("mostrar").get_active();
+        if self.wTree.get_widget("mostrar").get_active() == True :
+            check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido/usuario.conf","w")			
+            check_cb_conf.write("MOSTRAR=0")
+            check_cb_conf.close()
+        else:
+            check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido/usuario.conf","w")
+            check_cb_conf.write("MOSTRAR=1")
+            check_cb_conf.close()
 
-	def checkmostrar(self, widget):
-		print self.wTree.get_widget("mostrar").get_active();
-		if self.wTree.get_widget("mostrar").get_active() == True :
+    def b1_clicked(self, widget):
+        print "Abriendo Cunaguaro"
+        comando="cunaguaro"
+        os.system(comando+" &")
 
-			check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido/usuario.conf","w")			
-			check_cb_conf.write("MOSTRAR=0")
-			check_cb_conf.close()
+    def cerrar(self, widget,event=None):
+        print "Cerrando"
+        self.wTree.get_widget("window1").hide();
+        sys.exit(0)
 
-		else:
+    def b2_clicked(self, widget):
+        print "Abriendo Guacharo"
+        comando="guacharo"
+        os.system(comando+" &")
 
-			check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido/usuario.conf","w")
-			check_cb_conf.write("MOSTRAR=1")
-			check_cb_conf.close()
+    def b3_clicked(self, widget):
+        print "Abriendo Turpial"
+        comando="turpial"
+        os.system(comando+" &")
 
-	def b1_clicked(self, widget):
-                print "Abriendo Cunaguaro"
-		comando="cunaguaro"
-	        thr = threading.Thread(target= abrir_paralelo, args=(0,0,comando));
-                thr.start()
+    def b4_clicked(self, widget):
+        print "Abriendo Chat"
+        comando="pidgin"
+        os.system(comando+" &")
 
-	def b2_clicked(self, widget):
-                print "Abriendo Guacharo"
-		comando="guacharo"
-	        thr = threading.Thread(target= abrir_paralelo, args=(0,0,comando));
-                thr.start()
+    def b5_clicked(self, widget):
+        print "Abriendo LibreOffice"
+        comando="libreoffice"
+        os.system(comando+" &")
 
-	def b3_clicked(self, widget):
-                print "Abriendo Turpial"
-		comando="turpial"
-	        thr = threading.Thread(target= abrir_paralelo, args=(0,0,comando));
-                thr.start()
+    def b6_clicked(self, widget):
+        print "Abriendo GIMP"
+        comando="gimp"
+        os.system(comando+" &")
 
-        def b4_clicked(self, widget):
-                print "Abriendo Chat"
-                comando="pidgin"
-                thr = threading.Thread(target= abrir_paralelo, args=(0,0,comando));
-                thr.start()
+    def b7_clicked(self, widget):
+        print "Abriendo totem"
+        comando="totem"
+        os.system(comando+" &")
 
-	def b5_clicked(self, widget):
-                print "Abriendo LibreOffice"
-		comando="libreoffice"
-	        thr = threading.Thread(target= abrir_paralelo, args=(0,0,comando));
-                thr.start()
-
-	def b6_clicked(self, widget):
-                print "Abriendo GIMP"
-		comando="gimp"
-	        thr = threading.Thread(target= abrir_paralelo, args=(0,0,comando));
-                thr.start()
-
-	def b7_clicked(self, widget):
-                print "Abriendo VLC"
-		comando="vlc"
-	        thr = threading.Thread(target= abrir_paralelo, args=(0,0,comando));
-                thr.start()
-
-	def b8_clicked(self, widget):
-                print "Abriendo Exaile"
-		comando="exaile"
-	        thr = threading.Thread(target= abrir_paralelo, args=(0,0,comando));
-                thr.start()
+    def b8_clicked(self, widget):
+        print "Abriendo Exaile"
+        comando="exaile"
+        os.system(comando+" &")
 
 if __name__ == "__main__":
-        hwg = HellowWorldGTK()
+        hwg = canaimabienvenido()
         
         
