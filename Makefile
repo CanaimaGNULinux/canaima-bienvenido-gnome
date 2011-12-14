c2cb073d37f7dc842791491f59de2589eb4ecfb8
Sasha Veronica Solano Grosjean <sashasolano@gmail.com>
2011-12-14 11:29:47 -0430

    Se re hizo la aplicacion usando webkit ademas de corregir errores de lintian como unusual-interpreter ./usr/share/applications/canaima-bienvenido.desktop #!xdg-open , copyright-should-refer-to-common-license-file-for-gpl y canaima-bienvenido: executable-not-elf-or-script ./usr/share/canaima-bienvenido/interfaz.glade , se le agrego el archivo THANKS , se cambio el uploader del paquete

--- Makefile
+++ Makefile
@@ -35,7 +35,8 @@ install:
 	cp -r conf/usuario.conf $(DESTDIR)/etc/skel/.config/canaima-bienvenido-gnome/
 	cp -r desktop/canaima-bienvenido-gnome-automatico.desktop $(DESTDIR)/etc/skel/.config/autostart/
 	cp -r scripts/canaima-bienvenido-gnome.py $(DESTDIR)/usr/share/canaima-bienvenido-gnome/
-	cp -r scripts/interfaz.glade $(DESTDIR)/usr/share/canaima-bienvenido-gnome/
+	cp -r imagenes $(DESTDIR)/usr/share/canaima-bienvenido-gnome/
+	cp -r ui/interfaz.glade $(DESTDIR)/usr/share/canaima-bienvenido-gnome/
 	cp -r scripts/canaima-bienvenido-gnome.sh $(DESTDIR)/usr/bin/canaima-bienvenido-gnome
 	cp -r scripts/canaima-bienvenido-gnome-automatico.sh $(DESTDIR)/usr/bin/canaima-bienvenido-gnome-automatico
 
--- scripts/canaima-bienvenido-gnome.py
+++ scripts/canaima-bienvenido-gnome.py
@@ -1,387 +1,126 @@
-#!/usr/bin/env python
-
+#!/usr/bin/python
 import sys
+import pygtk
+pygtk.require("2.0")
+import gtk
+import gtk.glade
+import commands
+import os
+#import gettext
+from user import home
+import webkit
+import string
+import warnings
+warnings.filterwarnings('ignore')
 
-try:
-        import gtk
-        import os, threading, locale
-        import gobject
-        import pygtk
-        import vte
-	import webbrowser
-        pygtk.require("2.0")
-except:
-        pass
-try:
-        import gtk
-        import gtk.glade
-except:
-        sys.exit(1)
-
-class canaimabienvenido:
-    encoding = locale.getpreferredencoding()
-    utf8conv = lambda x : unicode(x, encoding).encode('utf8')
-
-    fuente = ""
-    destino = ""
-  
+class canaimabienvenido():
     def __init__(self):
-            #Set the Glade file
-            self.gladefile = "/usr/share/canaima-bienvenido-gnome/interfaz.glade"
-            self.wTree = gtk.glade.XML(self.gladefile) 
 
-            #Create our dictionay and connect it
-            dic = { "on_cerrar_clicked" : self.cerrar,
-                    "on_b1_clicked" : self.b1_clicked,
-                    "on_b2_clicked" : self.b2_clicked,
-                    "on_b3_clicked" : self.b3_clicked,
-                    "on_b4_clicked" : self.b4_clicked,
-                    "on_b5_clicked" : self.b5_clicked,
-                    "on_b6_clicked" : self.b6_clicked,
-                    "on_b7_clicked" : self.b7_clicked,
-                    "on_b8_clicked" : self.b8_clicked,
-                    "multimedia1" : self.multimedia1,
-                    "multimedia2" : self.multimedia2,
-                    "multimedia3" : self.multimedia3,
-                    "multimedia4" : self.multimedia4,
-                    "multimedia5" : self.multimedia5,
-                    "multimedia6" : self.multimedia6,
-                    "multimedia7" : self.multimedia7,
-                    "multimedia8" : self.multimedia8,
-                    "oficina1" : self.oficina1,
-                    "oficina2" : self.oficina2,
-                    "oficina3" : self.oficina3,
-                    "oficina4" : self.oficina4,
-                    "oficina5" : self.oficina5,
-                    "oficina6" : self.oficina6,
-                    "oficina7" : self.oficina7,
-                    "oficina8" : self.oficina8,
-                    "internet1" : self.internet1,
-                    "internet2" : self.internet2,
-                    "internet3" : self.internet3,
-                    "internet4" : self.internet4,
-                    "internet5" : self.internet5,
-                    "internet6" : self.internet6,
-                    "internet7" : self.internet7,
-                    "internet8" : self.internet8,
-                    "graficos1" : self.graficos1,
-                    "graficos2" : self.graficos2,
-                    "graficos3" : self.graficos3,
-                    "graficos4" : self.graficos4,
-                    "graficos5" : self.graficos5,
-                    "graficos6" : self.graficos6,
-                    "graficos7" : self.graficos7,
-                    "graficos8" : self.graficos8,
-                    "volver" : self.volver,
-                    "cambiar" : self.cambiar,
-                    "mostrar_internet" : self.mostrar_internet,
-                    "mostrar_graficos" : self.mostrar_graficos,
-                    "mostrar_multimedia" : self.mostrar_multimedia,
-                    "mostrar_oficina" : self.mostrar_oficina,
-                    "on_mostrar_toggled0" : self.checkmostrar0,
-                    "on_mostrar_toggled1" : self.checkmostrar1,
-                    "on_mostrar_toggled2" : self.checkmostrar2,
-                    "on_mostrar_toggled3" : self.checkmostrar3,
-                    "on_mostrar_toggled4" : self.checkmostrar4,
-                    "destroy" : self.cerrar,
-                    "cerrar" : self.cerrar,
-                    "on_MainWindow_destroy" : self.cerrar }
+        gladefile = "/usr/share/canaima-bienvenido-gnome/interfaz.glade"
+        wTree = gtk.glade.XML(gladefile,"main_window")
+        wTree.get_widget("main_window").set_title("Bienvenido a Canaima GNU Linux")
+        #wTree.get_widget("main_window").set_icon_from_file("/usr/share/canaima-bienvenido-gnome/templates/logo.svg")
+        wTree.get_widget("main_window").connect("destroy", gtk.main_quit)
+
+        browser = webkit.WebView()
+        wTree.get_widget("scrolled_welcome").add(browser)
+        browser.connect("button-press-event", lambda w, e: e.button == 3)
+        subs = {}
+        self.codecs_pkg = None
+        self.extra_pkg = None
+             
+        if os.path.exists(home + "/.canaima-bienvenido-gnome/norun.flag"):
+            subs['checked'] = ("")
+        else:
+            subs['checked'] = ("CHECKED")
             
-            self.wTree.get_widget("principal").show(); 
-            self.wTree.get_widget("multimedia").connect("delete-event",self.cerrar); 
-            self.wTree.get_widget("graficos").connect("delete-event",self.cerrar); 
-            self.wTree.get_widget("internet").connect("delete-event",self.cerrar); 
-            self.wTree.get_widget("oficina").connect("delete-event",self.cerrar); 
-            self.wTree.get_widget("principal").connect("delete-event",self.cerrar); 
-            
-            self.wTree.signal_autoconnect(dic)
-            gtk.main()
+        template = open("/usr/share/canaima-bienvenido-gnome/templates/welcome.html").read()
+        html = string.Template(template).safe_substitute(subs)
+        browser.load_html_string(html, "file:/")
+        browser.connect('title-changed', self.title_changed)
+        wTree.get_widget("main_window").show_all()
 
-    #Get the Main Window, and connect the "destroy" event
-
-    def volver(self,widget):
-        self.wTree.get_widget("principal").show()
-        self.wTree.get_widget("multimedia").hide()
-        self.wTree.get_widget("graficos").hide()
-        self.wTree.get_widget("oficina").hide()
-        self.wTree.get_widget("internet").hide()
-
-    def cambiar(self,widget):
-        view=self.wTree.get_widget("window2")
-        view.show()
-
-    def mostrar_internet(self,widget):
-        self.wTree.get_widget("multimedia").hide()
-        self.wTree.get_widget("graficos").hide()
-        self.wTree.get_widget("oficina").hide()
-        self.wTree.get_widget("internet").show()
-        self.wTree.get_widget("principal").hide()
-
-    def mostrar_oficina(self,widget):
-        self.wTree.get_widget("multimedia").hide()
-        self.wTree.get_widget("graficos").hide()
-        self.wTree.get_widget("oficina").show()
-        self.wTree.get_widget("internet").hide()
-        self.wTree.get_widget("principal").hide()
-
-    def mostrar_graficos(self,widget):
-        self.wTree.get_widget("multimedia").hide()
-        self.wTree.get_widget("graficos").show()
-        self.wTree.get_widget("oficina").hide()
-        self.wTree.get_widget("internet").hide()
-        self.wTree.get_widget("principal").hide()
-
-    def mostrar_multimedia(self,widget):
-        self.wTree.get_widget("multimedia").show()
-        self.wTree.get_widget("graficos").hide()
-        self.wTree.get_widget("oficina").hide()
-        self.wTree.get_widget("internet").hide()
-        self.wTree.get_widget("principal").hide()
- 
-    def checkmostrar(self, widget):
-        print self.wTree.get_widget("mostrar").get_active();
-        if self.wTree.get_widget("mostrar").get_active() == True :
-            check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido-gnome/usuario.conf","w")			
-            check_cb_conf.write("MOSTRAR=0")
-            check_cb_conf.close()
-        else:
-            check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido-gnome/usuario.conf","w")
-            check_cb_conf.write("MOSTRAR=1")
-            check_cb_conf.close()
-
-    def checkmostrar0(self, widget):
-        print self.wTree.get_widget("mostrar0").get_active();
-        if self.wTree.get_widget("mostrar0").get_active() == True :
-            check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido-gnome/usuario.conf","w")			
-            check_cb_conf.write("MOSTRAR=0")
-            check_cb_conf.close()
-        else:
-            check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido-gnome/usuario.conf","w")
-            check_cb_conf.write("MOSTRAR=1")
-            check_cb_conf.close()
-
-    def checkmostrar1(self, widget):
-        print self.wTree.get_widget("mostrar1").get_active();
-        if self.wTree.get_widget("mostrar1").get_active() == True :
-            check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido-gnome/usuario.conf","w")			
-            check_cb_conf.write("MOSTRAR=0")
-            check_cb_conf.close()
-        else:
-            check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido-gnome/usuario.conf","w")
-            check_cb_conf.write("MOSTRAR=1")
-            check_cb_conf.close()
-
-    def checkmostrar2(self, widget):
-        print self.wTree.get_widget("mostrar2").get_active();
-        if self.wTree.get_widget("mostrar2").get_active() == True :
-            check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido-gnome/usuario.conf","w")			
-            check_cb_conf.write("MOSTRAR=0")
-            check_cb_conf.close()
-        else:
-            check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido-gnome/usuario.conf","w")
-            check_cb_conf.write("MOSTRAR=1")
-            check_cb_conf.close()
-
-    def checkmostrar3(self, widget):
-        print self.wTree.get_widget("mostrar3").get_active();
-        if self.wTree.get_widget("mostrar3").get_active() == True :
-            check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido-gnome/usuario.conf","w")			
-            check_cb_conf.write("MOSTRAR=0")
-            check_cb_conf.close()
-        else:
-            check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido-gnome/usuario.conf","w")
-            check_cb_conf.write("MOSTRAR=1")
-            check_cb_conf.close()
-
-    def checkmostrar4(self, widget):
-        print self.wTree.get_widget("mostrar4").get_active();
-        if self.wTree.get_widget("mostrar4").get_active() == True :
-            check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido-gnome/usuario.conf","w")			
-            check_cb_conf.write("MOSTRAR=0")
-            check_cb_conf.close()
-        else:
-            check_cb_conf=open(os.environ['HOME']+"/.config/canaima-bienvenido-gnome/usuario.conf","w")
-            check_cb_conf.write("MOSTRAR=1")
-            check_cb_conf.close()
-
-    def b1_clicked(self, widget):
-        print "Abriendo Cunaguaro"
-        comando="cunaguaro"
-        os.system(comando+" &")
-
-    def cerrar(self, widget,event=None):
-        print "Cerrando"
-        self.wTree.get_widget("window1").hide();
-        sys.exit(0)
-
-    def b2_clicked(self, widget):
-        print "Abriendo Guacharo"
-        comando="guacharo"
-        os.system(comando+" &")
-
-    def b3_clicked(self, widget):
-        print "Abriendo Turpial"
-        comando="turpial"
-        os.system(comando+" &")
-
-    def b4_clicked(self, widget):
-        print "Abriendo Chat"
-        comando="pidgin"
-        os.system(comando+" &")
-
-    def b5_clicked(self, widget):
-        print "Abriendo LibreOffice"
-        comando="libreoffice"
-        os.system(comando+" &")
-
-    def b6_clicked(self, widget):
-        print "Abriendo GPaint"
-        comando="gpaint"
-        os.system(comando+" &")
-
-    def b7_clicked(self, widget):
-        print "Abriendo totem"
-        comando="totem"
-        os.system(comando+" &")
-
-    def b8_clicked(self, widget):
-        print "Abriendo Exaile"
-        comando="exaile"
-        os.system(comando+" &")
-
-    def internet1(self, widget):
-        comando="cunaguaro"
-        os.system(comando+" &")
-
-    def internet2(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def internet3(self, widget):
-        comando="guacharo"
-        os.system(comando+" &")
-
-    def internet4(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def internet5(self, widget):
-        comando="turpial"
-        os.system(comando+" &")
-
-    def internet6(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def internet7(self, widget):
-        comando="pidgin"
-        os.system(comando+" &")
-
-    def internet8(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-
-    def oficina1(self, widget):
-        comando="libreoffice -writer"
-        os.system(comando+" &")
-
-
-    def oficina2(self, widget):
-        comando="libreoffice -impress"
-        os.system(comando+" &")
-
-    def oficina3(self, widget):
-        comando="libreoffice -calc"
-        os.system(comando+" &")
-
-    def oficina4(self, widget):
-        comando="libreoffice"
-        os.system(comando+" &")
-
-    def oficina5(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def oficina6(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def oficina7(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def oficina8(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def graficos1(self, widget):
-        comando="shotwell"
-        os.system(comando+" &")
-
-    def graficos2(self, widget):
-        comando="libreoffice -draw"
-        os.system(comando+" &")
-
-    def graficos3(self, widget):
-        comando="simple-scan"
-        os.system(comando+" &")
-
-    def graficos4(self, widget):
-        comando="gpaint"
-        os.system(comando+" &")
-
-    def graficos5(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def graficos6(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def graficos7(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def graficos8(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def multimedia1(self, widget):
-        comando="gnome-sound-recorder"
-        os.system(comando+" &")
-
-    def multimedia2(self, widget):
-        comando="totem"
-        os.system(comando+" &")
-
-    def multimedia3(self, widget):
-        comando="exaile"
-        os.system(comando+" &")
-
-    def multimedia4(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def multimedia5(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def multimedia6(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def multimedia7(self, widget):
-        comando=""
-        os.system(comando+" &")
-
-    def multimedia8(self, widget):
-        comando=""
-        os.system(comando+" &")
-
+    def title_changed(self, view, frame, title):
+        if title.startswith("nop"):
+            return
+        # call directive looks like:
+        #  "call:func:arg1,arg2"
+        #  "call:func"
+        if title == "event_irc":
+            if os.path.exists("/usr/bin/xchat-gnome"):
+                os.system("/usr/bin/xchat-gnome &")
+            elif os.path.exists("/usr/bin/xchat"):
+                os.system("/usr/bin/xchat &")
+            elif os.path.exists("/usr/bin/konversation"):
+                os.system("/usr/bin/konversation &")
+            elif os.path.exists("/usr/bin/quassel"):
+                os.system("/usr/bin/quassel &")
+        elif title == "cunaguaro":
+            comando="cunaguaro"
+            os.system(comando+" &")
+        elif title == "abrir_wiki":
+            os.system("xdg-open http://wiki.canaima.softwarelibre.gob.ve/")
+        elif title == "abrir_portal":
+            os.system("xdg-open http://canaima.softwarelibre.gob.ve/")
+        elif title == "guacharo":
+            comando="guacharo"
+            os.system(comando+" &")
+        elif title == "turpial":
+            comando="turpial"
+            os.system(comando+" &")
+        elif title == "xchat":
+            comando="pidgin"
+            os.system(comando+" &")
+        elif title == "shotwell":
+            comando="shotwell"
+            os.system(comando+" &")
+        elif title == "escaner":
+            comando="simple-scan"
+            os.system(comando+" &")
+        elif title == "libreof":
+            comando="libreoffice -draw"
+            os.system(comando+" &")
+        elif title == "gpaint":
+            comando="gpaint"
+            os.system(comando+" &")
+        elif title == "procesador_texto":
+            comando="libreoffice -writer"
+            os.system(comando+" &")
+        elif title == "hoja_calculo":
+            comando="libreoffice -calc"
+            os.system(comando+" &")        
+        elif title == "impres":
+            comando="libreoffice -impress"
+            os.system(comando+" &")           
+        elif title == "ofimatica":
+            comando="libreoffice"
+            os.system(comando+" &")
+        elif title == "sonidos":
+            comando="gnome-sound-recorder"
+            os.system(comando+" &")        
+        elif title == "toten":
+            comando="totem"
+            os.system(comando+" &")      
+        elif title == "exaile":
+            comando="exaile"
+            os.system(comando+" &")
+        elif title == "event_close_true":
+            if os.path.exists(home + "/.linuxmint/mintWelcome/norun.flag"):
+                os.system("rm -rf " + home + "/.linuxmint/mintWelcome/norun.flag")
+            gtk.main_quit()
+        elif title == "event_close_false":
+            os.system("mkdir -p " + home + "/.canaima-bienvenido-gnome")
+            os.system("touch " + home + "/.canaima-bienvenido-gnome/norun.flag")
+            gtk.main_quit()
+        elif title == "checkbox_checked":
+            if os.path.exists(home + "/.canaima-bienvenido-gnome/norun.flag"):
+                os.system("rm -rf " + home + "/.canaima-bienvenido-gnome/norun.flag")
+        elif title == "checkbox_unchecked":
+            os.system("mkdir -p " + home + "/.canaima-bienvenido-gnome")
+            os.system("touch " + home + "/.canaima-bienvenido-gnome/norun.flag")
 
 
 if __name__ == "__main__":
-        hwg = canaimabienvenido()
-        
+    canaimabienvenido()
+    gtk.main()
         
